df9fba78d85ea9494d4a7cff666123bf12e93cbc
gdgeek <dirui1@yeah.net>
2024-09-08 12:33:07 +0800

    Revert "no message"
    This reverts commit 8043d84cb3d1914be057a2088ee4bfe1b1050b11.

--- Plugins/GDGeek/Running/Story/ITaskByName.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-namespace GDGeek { 
-    public interface ITaskByName  {
-        Task getTask(string name);
-    }
-}
--- Plugins/GDGeek/State/Chart/Message/JsonMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-namespace GDGeek { 
-    public class JsonMessage : TypeMessage<string>
-    {
-
-    }
-}
